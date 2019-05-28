cc5ee8db08d5f8f8d91eef3febbc46fe291afd87
Matt <mbeimcik@gmail.com>
2019-05-28 19:49:14 +0200

    Add UI for adding new item

--- app/src/main/java/com/leadinsource/earley/MainActivity.kt
+++ app/src/main/java/com/leadinsource/earley/MainActivity.kt
@@ -1,22 +1,34 @@
 package com.leadinsource.earley
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.core.view.isNotEmpty
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
-class MainActivity : AppCompatActivity(), RecyclerViewAdapter.Callback {
+class MainActivity : AppCompatActivity(), RecyclerViewAdapter.Callback,
+    AddProjectFragment.OnFragmentInteractionListener {
+
+    lateinit var addProjectFragment: Fragment
+
+    override fun onFragmentInteraction(project: Project) {
+        viewModel.addProject(project)
+        removeAddProjectFragment()
+        //TODO animation instead of toast
+        Toast.makeText(this, "Adding new project", Toast.LENGTH_SHORT).show()
+    }
 
     // Let's try to implement as much as possible from https://developer.android.com/jetpack
 
     lateinit var viewModel: MainActivityViewModel
     private lateinit var viewAdapter: RecyclerViewAdapter
     private var recyclerView: RecyclerView? = null
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,12 +54,40 @@ class MainActivity : AppCompatActivity(), RecyclerViewAdapter.Callback {
         }
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.addProject -> {
+                displayNewProjectDialog()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun displayNewProjectDialog() {
+        //
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        addProjectFragment = AddProjectFragment.newInstance()
+        fragmentTransaction.add(R.id.rootLayout, addProjectFragment)
+        fragmentTransaction.commit()
+    }
 
     override fun onClick(position: Int) {
+        viewModel.setItemDone(position)
 
-       viewModel.setItemDone(position)
+    }
 
-
-
+    private fun removeAddProjectFragment() {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.remove(addProjectFragment)
+        fragmentTransaction.commit()
     }
 }
