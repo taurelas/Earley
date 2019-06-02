f6f9619f39f26cc5b5c03b10b75de39d0146accf
Matt <mbeimcik@gmail.com>
2019-06-02 13:42:09 +0200

    Add info field to Project

--- app/src/main/java/com/leadinsource/earley/Project.kt
+++ app/src/main/java/com/leadinsource/earley/Project.kt
@@ -1,3 +1,3 @@
 package com.leadinsource.earley
 
-class Project(val uuid: Int = 0, var name: String, var position:Int)
+class Project(val uuid: Int = 0, var name: String, var position:Int, var info: String = "")
